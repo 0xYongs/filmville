e20b115175be0ab53578628efcf1365be0b93093
0xYongs <tradingsensei21@gmail.com>
2023-05-30 19:38:16 +0200

    Add props(properties) to my person function and call them to my App function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 
-const Person = () => {
+const Person = (props) => {
   return (
     <>
-    <h1>Name : Yongs </h1>
-    <h2>Occupation : SWE </h2>
-    <h2>Age : 23</h2>
+     <h1>Name : {props.name} </h1>
+     <h2>Occupation : {props.occupation} </h2>
+     <h2>Age : {props.age}</h2>
     </>
   );
 }
@@ -13,7 +13,12 @@ const Person = () => {
 const App = () => {
   return (
       <div className="App">
-        <Person />
+        <Person name = {'Yongz'} occupation = {'SDE'} age = {'20'}/>
+
+        <Person 
+          name = {'Yongs'}
+          occupation = {'Full Stack Engineer'}
+          age = {'23'}/>
       </div>
   );
 }
