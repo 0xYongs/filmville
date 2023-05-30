c6f4ed7174705da4d67380c3463a4b9516409b2f
0xYongs <tradingsensei21@gmail.com>
2023-05-30 20:38:40 +0200

    Create a simple counter app by utilizing button onClick events

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ const App = () => {
   const [counter, setCounter] = useState(0);
   return (
       <div className="App">
-        <button onClick={() => alert('clicked')}>+</button>
+        <button onClick={() => setCounter(prevCount => prevCount + 1)}>+</button>
         <h1>{counter}</h1>
-        <button>-</button>
+        <button onClick={() => setCounter(prevCount => prevCount - 1)}>-</button>
       </div>
   );
 }
