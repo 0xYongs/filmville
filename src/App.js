21257ebaf057abf98808bec7d5025f3722e5c305
0xYongs <tradingsensei21@gmail.com>
2023-05-30 19:15:49 +0200

    Create my first component function and call it on App function

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import './App.css';
 
-function App() {
+const Person = () => {
+  return (
+    <>
+    <h1>Name : Yongs </h1>
+    <h2>Occupation : SWE </h2>
+    <h2>Age : 23</h2>
+    </>
+  );
+}
 
-  const name = null;
-  const myname = 'Yongs';
-  
+const App = () => {
   return (
       <div className="App">
-        <h1>Hello, {myname}</h1>
-        {name ? (
-          <>
-            <h1>{myname}</h1>
-            <h2>ON true</h2>
-          </>
-        ) : (
-          <>
-            <h1> Yongs Showing on null </h1>
-            <h2> OR on false</h2>
-          </>
-        )}
+        <Person />
       </div>
   );
 }
