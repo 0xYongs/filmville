b9ced124d137915080f1b97ec3f59150b19dbfe2
0xYongs <tradingsensei21@gmail.com>
2023-05-30 18:02:17 +0200

    Work on different ways of outputting the same answer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 
 function App() {
+
+  const name = 'Yongs'
   return (
       <div className="App">
-        <h1>Hello, React! (yongs) 
-          27/5/23 4:02am</h1>
+        <h1>Hello, {name}! (yongs) 27/5/23 4:02am</h1>
       </div>
   );
 }
