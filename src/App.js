d9a89a74ab431ca2112ee6542a2a8540754eaa41
0xYongs <tradingsensei21@gmail.com>
2023-06-09 19:51:30 +0200

    Create new index.js and App.js files to begin my filmville project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import React from 'react';
+import { useEffect } from 'react';
+
+// 7adc9cb7
+const API_URL = 'http://www.omdbapi.com?apikey=7adc9cb7';
 
 const App = () => {
+    
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}) & s=${title}`);
+        const data = await response.json();
+
+        console.log(data);
+    }
+
+    useEffect(() => {
+        searchMovies('Spiderman');
+    }, []);
+
     return(
         <h1>App</h1>
     );
