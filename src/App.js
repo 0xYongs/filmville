9e40476af43a68d68888adeef3917f866164c15e
0xYongs <tradingsensei21@gmail.com>
2023-06-09 23:47:12 +0200

    Remove unused movie1 attribute and modify files MovieCard and search.svg

--- src/App.js
+++ src/App.js
@@ -8,14 +8,6 @@ import MovieCard from './MovieCard';
 // 82607004
 const API_URL = 'http://www.omdbapi.com?apikey=82607004';
 
-const movie1 = {
-    "Title": "Italian Spiderman",
-    "Year": "2007",
-    "imdbID": "tt2705436",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BZWQxMjcwNjItZjI0ZC00ZTc4LWIwMzItM2Q0YTZhNzI3NzdlXkEyXkFqcGdeQXVyMTA0MTM5NjI2._V1_SX300.jpg"
-    }
-
 const App = () => {
 
     const [movies, setMovies] = useState([]);
